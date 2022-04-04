5abf420d70814e8dfc5901d1868f1aea736c9313
wandbergdelima <wandberg@gmail.com>
2022-04-03 22:03:39 -0300

    continuação dos estudos Js Parte 2

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,33 @@
-console.log("Fui carregado de um arquivo externo");
+var paciente = document.querySelector("#primeiro-paciente");
 
-var titulo = document.querySelector(".titulo");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdImc = paciente.querySelector(".info-imc");
 
-titulo.textContent = "Aparecida Nutricionista";
+
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if (peso <= 0 || peso > 1000) {
+    console.log("Peso inválido!");
+    tdPeso.textContent = "Peso inválido!";
+    pesoEhValido = false;
+
+}
+
+if (altura <= 0 || altura >= 3) {
+    console.log("Altura inválida!");
+    tdAltura.textContent = "Altura inválida!";
+    alturaEhValida = false;
+}
+
+if (alturaEhValida && pesoEhValido) {
+
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
+}
