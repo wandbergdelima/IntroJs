d1f6aa7cfe22d903679a7cdcb2855f6f09758f8c
wandbergdelima <wandberg@gmail.com>
2022-07-03 22:59:32 -0300

    implementação da requisição assíncrona com AJAX

--- js/form.js
+++ js/form.js
@@ -4,25 +4,28 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     var form = document.querySelector("#form-adiciona");
     var paciente = obtemPacienteDoFormulario(form);
-    var pacienteTr = montaTr(paciente);
+    
     var erros = validaPaciente(paciente);
 
     if (erros.length > 0) {
         exibeMensagensDeErro(erros);
-
         return;
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
+    // Chamando a função para buscar os pacientes de dentro da API 
+    adicionaPacienteNaTabela(paciente);
+    
+    
     form.reset();
-
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
-
 });
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form) {
 
