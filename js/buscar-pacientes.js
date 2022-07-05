b1877f24a0712802e9d9e36cf016562cded4edec
wandbergdelima <wandberg@gmail.com>
2022-07-04 21:41:51 -0300

    conclusão do projeto Aparecida Nutricionista

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -3,18 +3,28 @@ var botaoAdicionar = document.querySelector('#buscar-pacientes')
 botaoAdicionar.addEventListener("click", function(){
     console.log("Buscando Pacientes");
     
-    var api = new XMLHttpRequest(); // 
-    api.open("GET", "https://api-pacientes.herokuapp.com/pacientes") // passando o methodo e URL a ser acessada
-    api.addEventListener("load", function(){
-        // Buscando a resposta da API como STRING(texto)
-        var resposta = api.responseText;
+    var api = new XMLHttpRequest(); // iniciando a requisição XML
 
-        // exibindo resposta da API em um ARRAY de (Objetos)
-        var pacientes = JSON.parse(resposta);
-        
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente)
-        });
+    api.open("GET", "https://api-pacientes.herokuapp.com/pacientes") // passando o methodo e URL a ser acessada
+    
+    api.addEventListener("load", function(){
+        var erroAjax = document.querySelector("#erro-ajax");
+        if (api.status == 200) {
+            erroAjax.classList.add("invisivel");
+            // Buscando a resposta da API como STRING(texto)
+            var resposta = api.responseText;
+            // exibindo resposta da API em um ARRAY de (Objetos)
+            var pacientes = JSON.parse(resposta);
+    
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        }else{
+            console.log(api.status);
+            console.log(api.responseText);
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.remove("invisivel");
+        }
     });
     api.send();
 });
